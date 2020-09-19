750d1a26f142c2290bba6efa6c8c62514bd32ddd
Alessandro <alessandroadm@live.com>
2020-09-19 20:56:14 -0300

    chore: initial settings and install dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single']
+  }
+};
